932e1ed3667467fe81fe6779776687bedb52c6f5
Timo Pirn <71009150+TimTimDev@users.noreply.github.com>
2024-02-21 16:59:13 +0200

    Added styling and functioning entry fields
    Added grid styling for elements and added functions to handle entry fields data

--- timer/timer.py
+++ timer/timer.py
@@ -2,45 +2,69 @@ from playsound import playsound
 import tkinter as tk
 import tkinter.ttk as ttk
 
-count = 60
+
 running = False
 
-def reset():
+def get_count():
     global count
-    count = 60
-    sec_label['text'] = 'Time Left: ' + str(count)
-    window.update()
+    seconds = sec_entry.get()
+    minutes = min_entry.get()
+    count = int(minutes) * 60 + int(seconds)
 
 def start_counter():
     global running
     running = True
+    get_count()
+
+def reset():
+    global count
+    get_count()
+    show_time['text'] = 'Time Left: ' + str(count)
+    window.update()
+
+def stop_counter():
+    global running
+    running = False
         
 def counter(): 
     global count
     global running
     if running:
         count -= 1
-        sec_label['text'] = 'Time Left: ' + str(count)
+        show_time['text'] = 'Time Left: ' + str(count)
         window.update()
-        if count == 0:
+        if count <= 0:
             playsound("C:/Users/timoo/OneDrive/Töölaud/timer/timer_alarm.mp3")
             reset()
             stop_counter()
     window.after(1000, counter)
 
-def stop_counter():
-    global running
-    running = False
-
 
 window = tk.Tk()
+window.title("Timer")
+window.geometry("300x200")
+
+window.columnconfigure([0, 1, 2, 3, 4, 5], minsize=50, pad=5)
+window.rowconfigure([0, 1, 2, 3], minsize=50, pad=5)
+
 sec_label = tk.Label(text="Sec")
 sec_entry = ttk.Entry(width=5)
+sec_entry.insert(0, 0)
+sec_label.grid(row=0, column=3, columnspan=2)
+sec_entry.grid(row=1, column=3, columnspan=2)
+
 min_label = tk.Label(text="Min")
 min_entry = ttk.Entry(width=5)
+min_entry.insert(0, 0)
+min_label.grid(row=0, column=1, columnspan=2)
+min_entry.grid(row=1, column=1, columnspan=2)
+
+show_time = tk.Label(text="Time Left: ")
+show_time.grid(row=2, column=2, columnspan=2)
+
 start_button = ttk.Button(
     text="Start",
-    width=10, 
+    width=10,
     command=start_counter
 )
 stop_button = ttk.Button(
@@ -53,14 +77,11 @@ reset_button = ttk.Button(
     width=10, 
     command=reset
 )
-sec_label.pack()
-sec_entry.pack()
-min_label.pack()
-min_entry.pack()
-start_button.pack()
-stop_button.pack()
-reset_button.pack()
-seconds = sec_entry.get()
-minutes = min_entry.get()
+
+
+start_button.grid(row=3, column=0, columnspan=2)
+stop_button.grid(row=3, column=2, columnspan=2)
+reset_button.grid(row=3, column=4, columnspan=2)
+
 window.after(1000, counter)
 window.mainloop()
